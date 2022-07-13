ad47738f22ba0e9b1d349930b5384babef9ff84c
VadimChubar <chvagr@gmail.com>
2022-07-13 12:47:48 +0300

    update rec func

--- rec_func.py
+++ rec_func.py
@@ -1,18 +1,16 @@
-# from pathlib import Path
-# import sys
-# p = Path(sys.argv[1])
+from pathlib import Path
+import sys
+p = Path(sys.argv[1])
 
 
-# def parse_folder_recursion(path):
-#     for element in path.iterdir():
-#         if element.is_dir():
-#             print(f'This is folder - {element.name}')
-#             parse_folder_recursion(element)
-#         else:
-#             print(f'This is file - {element.name}')
+def parse_folder_recursion(path):
+    for element in path.iterdir():
+        if element.is_dir():
+            print(f'This is folder - {element.name}')
+            parse_folder_recursion(element)
+        else:
+            print(f'This is file - {element.name}')
 
 
-# if __name__ == '__main__':
-#     parse_folder_recursion(p)
-
-print('hello world!')
+if __name__ == '__main__':
+    parse_folder_recursion(p)
