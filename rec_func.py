52694e3402efbb48c6ef7d887b954d5b71e119e8
VadimChubar <chvagr@gmail.com>
2022-07-13 12:45:44 +0300

    create rec func
    створення рекурсивної ункції

---
+++ rec_func.py
@@ -0,0 +1,16 @@
+# from pathlib import Path
+# import sys
+# p = Path(sys.argv[1])
+
+
+# def parse_folder_recursion(path):
+#     for element in path.iterdir():
+#         if element.is_dir():
+#             print(f'This is folder - {element.name}')
+#             parse_folder_recursion(element)
+#         else:
+#             print(f'This is file - {element.name}')
+
+
+# if __name__ == '__main__':
+#     parse_folder_recursion(p)
